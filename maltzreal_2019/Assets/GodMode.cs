e286ed8d4de343002e09d4acbb917cf5ec105fac
anaammostafiz <anaam.mostafiz@gmail.com>
2021-02-25 14:03:33 -0500

    added all functioning model buttons

--- maltzreal_2019/Assets/GodMode.cs
+++ maltzreal_2019/Assets/GodMode.cs
@@ -7,6 +7,24 @@ public class GodMode : MonoBehaviour
     public GameObject FPC;
     public GameObject model_1B;
     public GameObject model_2B;
+    public GameObject model_3B;
+    public GameObject model_4B;
+    public GameObject model_5B;
+    public GameObject model_6B;
+    public GameObject model_12B;
+    public GameObject model_17B;
+    public GameObject model_18B;
+    public GameObject model_19B;
+    public GameObject model_20B;
+    public GameObject model_21B;
+    public GameObject model_22B;
+    public GameObject model_23B;
+    public GameObject model_24B;
+    public GameObject model_51B;
+    public GameObject model_55B;
+    public GameObject model_61B;
+    public GameObject model_67B;
+    public GameObject model_68B;
 
     public void Spawn1B()
     {
@@ -27,4 +45,185 @@ public class GodMode : MonoBehaviour
         Paws.GameIsPaws = false;
         FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
     }
+
+    public void Spawn3B()
+    {
+        GameObject Clone = Instantiate(model_3B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn4B()
+    {
+        GameObject Clone = Instantiate(model_4B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn5B()
+    {
+        GameObject Clone = Instantiate(model_5B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn6B()
+    {
+        GameObject Clone = Instantiate(model_6B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn12B()
+    {
+        GameObject Clone = Instantiate(model_12B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn17B()
+    {
+        GameObject Clone = Instantiate(model_17B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn18B()
+    {
+        GameObject Clone = Instantiate(model_18B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn19B()
+    {
+        GameObject Clone = Instantiate(model_19B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn20B()
+    {
+        GameObject Clone = Instantiate(model_20B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn21B()
+    {
+        GameObject Clone = Instantiate(model_21B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn22B()
+    {
+        GameObject Clone = Instantiate(model_22B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn23B()
+    {
+        GameObject Clone = Instantiate(model_23B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn24B()
+    {
+        GameObject Clone = Instantiate(model_24B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn51B()
+    {
+        GameObject Clone = Instantiate(model_51B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn55B()
+    {
+        GameObject Clone = Instantiate(model_55B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn61B()
+    {
+        GameObject Clone = Instantiate(model_61B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn67B()
+    {
+        GameObject Clone = Instantiate(model_67B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
+
+    public void Spawn68B()
+    {
+        GameObject Clone = Instantiate(model_68B);
+        Clone.transform.position = new Vector3(0, 0, 0);
+        Mooodel.GameIsModel = false;
+        Time.timeScale = 1f;
+        Paws.GameIsPaws = false;
+        FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.m_cursorIsLocked = true;
+    }
 }
+
