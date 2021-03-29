5073546c2b7564c7b7ce568992fa38b049df3a7b
anaammostafiz <anaam.mostafiz@gmail.com>
2021-03-29 19:11:49 -0400

    start menu added

---
+++ maltzreal_2019/Assets/BootUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootUp : MonoBehaviour
+{
+    public GameObject BootMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BootMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            BootMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
